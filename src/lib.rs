bb7194bd46060f4b6d12ae875bc78a47fe58f0ae
lilpum <lilpum@qq.com>
2024-03-13 01:40:21 +0800

    feat(css): 添加切换主题功能

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,24 @@
-use leptos::{component, view, IntoView, create_signal, SignalUpdate, SignalGet};
-use leptos_meta::{provide_meta_context, Link, Meta, Stylesheet, Title};
-use leptos_router::{Route, Router, Routes, A};
-use crate::pages::not_found::NotFoundPage;
+use leptos::{component, create_rw_signal, create_signal, IntoView, provide_context, ReadSignal, RwSignal, SignalGet, SignalUpdate, use_context, view, WriteSignal};
+use leptos_meta::{Link, Meta, provide_meta_context, Stylesheet, Title};
+use leptos_router::{A, Route, Router, Routes};
 
+use crate::css::CssClass::{AboutContainer, AboutH1, AboutP, Container, HomeButton, HomeContainer, HomeH2, HomeP, MyFooter, Nav, NavLink};
+use crate::css::Theme;
+use crate::pages::not_found::NotFoundPage;
+use crate::state::GlobalState;
 
 mod components;
 mod pages;
+mod css;
+mod state;
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
+
+    let state = create_rw_signal(GlobalState::default());
+    provide_context(state);
+
     view! {
         <Stylesheet id="leptos" href="/style/output.css"/>
         <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
@@ -20,7 +29,7 @@ pub fn App() -> impl IntoView {
         <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
         <Router>
-             <div class="flex flex-col min-h-screen bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100">
+             <div class={move || Container.get_css(state.get().theme)}>
                 <NavBar/>
                 <main class="flex-grow">
                     <Routes>
@@ -38,16 +47,17 @@ pub fn App() -> impl IntoView {
 
 #[component]
 fn NavBar() -> impl IntoView {
+    let state = use_context::<RwSignal<GlobalState>>().unwrap_or_default();
     view! {
-        <nav class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+        <nav class={move || Nav.get_css(state.get().theme)}>
             <ul class="flex justify-center space-x-10 py-4">
                 <li>
-                    <A href="/" class="nav-link font-bold text-lg hover:bg-indigo-700 p-3 rounded-lg transition-all duration-300">
+                    <A href="/" class={move || NavLink.get_css(state.get().theme)}>
                         {"Home"}
                     </A>
                 </li>
                 <li>
-                    <A href="/about" class="nav-link font-bold text-lg hover:bg-indigo-700 p-3 rounded-lg transition-all duration-300">
+                    <A href="/about" class={move || NavLink.get_css(state.get().theme)}>
                         {"About"}
                     </A>
                 </li>
@@ -58,8 +68,9 @@ fn NavBar() -> impl IntoView {
 
 #[component]
 fn Footer() -> impl IntoView {
+    let state = use_context::<RwSignal<GlobalState>>().unwrap_or_default();
     view! {
-        <footer class="mt-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white text-center py-4">
+        <footer class={move || MyFooter.get_css(state.get().theme)}>
             <p class="font-bold">{"© 2024 Company Name. All rights reserved."}</p>
         </footer>
     }
@@ -67,10 +78,11 @@ fn Footer() -> impl IntoView {
 
 #[component]
 fn About() -> impl IntoView {
+    let state = use_context::<RwSignal<GlobalState>>().unwrap_or_default();
     view! {
-        <div class="text-center p-10">
-            <h1 class="text-3xl font-bold my-4">{"About Us"}</h1>
-            <p class="mt-2 text-lg">{"Here you can learn more about us."}</p>
+        <div class={move || AboutContainer.get_css(state.get().theme)}>
+            <h1 class={move || AboutH1.get_css(state.get().theme)}>{"About Us"}</h1>
+            <p class={move || AboutP.get_css(state.get().theme)}>{"Here you can learn more about us."}</p>
         </div>
     }
 }
@@ -78,15 +90,15 @@ fn About() -> impl IntoView {
 
 #[component]
 pub fn Home() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-
+    let state = use_context::<RwSignal<GlobalState>>().unwrap_or_default();
+    let (count, set_count) = create_signal(1);
     view! {
-        <div class="my-0 mx-auto max-w-3xl text-center">
-            <h2 class="p-6 text-4xl font-bold">"Welcome to Leptos with Tailwind"</h2>
-            <p class="px-10 pb-10 text-left text-lg">"Tailwind will scan your Rust files for Tailwind class names and compile them into a CSS file."</p>
+        <div class={move || HomeContainer.get_css(state.get().theme)}>
+            <h2 class={move || HomeH2.get_css(state.get().theme)}>"Welcome to Leptos with Tailwind"</h2>
+            <p class={move || HomeP.get_css(state.get().theme)}>"Tailwind will scan your Rust files for Tailwind class names and compile them into a CSS file."</p>
             <button
-                class="bg-gradient-to-r from-pink-500 to-yellow-500 hover:from-pink-600 hover:to-yellow-600 active:to-pink-700 active:from-yellow-500 px-6 py-3 text-white rounded-full shadow-md transform active:scale-95 transition-all duration-300"
-                on:click=move |_| set_count.update(|count| *count += 1)
+                class={move || HomeButton.get_css(state.get().theme)}
+                on:click=move |_| home_button_on_click(count,set_count,state)
             >
                 "Something's here | "
                 {move || if count.get() == 0 {
@@ -100,3 +112,13 @@ pub fn Home() -> impl IntoView {
     }
 }
 
+/// 增加计数器 + 切换主题
+fn home_button_on_click(count: ReadSignal<i32>, set_count: WriteSignal<i32>, state: RwSignal<GlobalState>) {
+    set_count.update(|count| *count += 1);
+    let current_count = count.get();
+    if current_count % 2 == 0 {
+        state.update(|state| state.theme = Theme::Twitter);
+    } else {
+        state.update(|state| state.theme = Theme::Ig);
+    }
+}
--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,13 +1,16 @@
-use leptos::{component, IntoView, view};
+use leptos::{component, IntoView, RwSignal, SignalGet, use_context, view};
 use leptos_router::A;
+use crate::css::CssClass::{NotFoundPageA, NotFoundPageContainer, NotFoundPageH1, NotFoundPageP};
+use crate::state::GlobalState;
 
 #[component]
 pub fn NotFoundPage() -> impl IntoView {
+    let state = use_context::<RwSignal<GlobalState>>().unwrap_or_default();
     view! {
-        <div class="m-auto text-center p-20 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg shadow-xl">
-            <h1 class="text-9xl font-bold text-white mb-8">"404"</h1>
-            <p class="text-2xl text-white mb-8">"抱歉，我们找不到您请求的页面。"</p>
-            <A href="/" class="px-8 py-3 text-lg font-semibold rounded-full bg-white text-indigo-500 hover:bg-gray-200 transition-colors duration-300">"返回首页"</A>
+        <div class={move || NotFoundPageContainer.get_css(state.get().theme)}>
+            <h1 class={move || NotFoundPageH1.get_css(state.get().theme)}>"404"</h1>
+            <p class={move || NotFoundPageP.get_css(state.get().theme)}>"抱歉，我们找不到您请求的页面。"</p>
+            <A href="/" class={move || NotFoundPageA.get_css(state.get().theme)}>"返回首页"</A>
         </div>
     }
 }
